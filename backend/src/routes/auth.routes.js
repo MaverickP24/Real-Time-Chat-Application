844a7cce968b34348ad9cf79d7962713e3fb3700
) <pratyushparida36@gmail.com>
2025-04-08 02:12:14 +0530

    User Model Completed

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,14 +1,21 @@
 import express from "express" ;
 
-import { signup,login,logout } from "../controllers/auth.controller.js";
+import { signup,login,logout,updateProfile ,checkAuth} from "../controllers/auth.controller.js";
 
-const router = express.Router()
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+
+
+
+let router = express.Router()
 
 router.post("/signup",signup)
 router.post("/login",login)
 router.post("/logout",logout)
 
-// router.put("/update-profile",protectRoute,updateProfile)
+router.put("/update-profile",protectRoute,updateProfile)
+
+router.get("/check",protectRoute,checkAuth);
 
 // module.exports = router
 
